c2929b667204ab674938b278bdc1e5c578ef1025
s.ebrahimzadeh <s.ebrahimzadeh@zi-tel.com>
2025-03-12 13:14:37 +0330

    added fetching data

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,6 +1,6 @@
 import {FC} from "react";
-import { useQuery } from 'react-query';
-import api from "../api/api.ts";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import api from "../api/api";
 
 type PostProps = {
     id?: number,
@@ -9,26 +9,52 @@ type PostProps = {
     body?: string,
 }
 
-const MyComponent: FC<PostProps> = () => {
+const fetchPosts = async ():Promise<PostProps[]> => {
+    const { data } = await api.get("/posts");
+    return data;
+};
 
-    const { data } = useQuery('posts', async () => {
-        const { data } = await api.get("/posts");
-        return data;
-    })
+const addPost = async (newPost: Omit<PostProps, "id">) => {
+    const { data } = await api.post("/posts", newPost);
+    return data;
+};
+
+const MyComponent: FC<PostProps> = () => {
+    const queryClient = useQueryClient();
+
+    const { data, error, isLoading } = useQuery<PostProps[]>({
+        queryKey: ["posts"],
+        queryFn: fetchPosts,
+    });
+
+    const addMutation = useMutation({
+        mutationFn: addPost,
+        onMutate: () => console.log("sending data..."),
+        onSuccess: () => {
+            console.log("post successfully added.");
+            queryClient.invalidateQueries(["posts"]);
+        },
+        onError: (error) => console.error("error", error),
+    });
+
+    if (isLoading) return <p>Loading...</p>;
+    if (error instanceof Error) return <p>Error: {error.message}</p>;
 
     return (
         <div>
-            <h2>Posts</h2>
+            <h1>Posts</h1>
+            <button
+                onClick={() => addMutation.mutate({userId: 7, title: "New Post", body: "Hello"})}
+            >
+                Add Post
+            </button>
             <ul>
                 {data?.map((post: PostProps) => (
-                    <li key={post.id}>
-                        {post.title}
-                    </li>
+                    <li key={post.id}>{post.title}</li>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
 
 export default MyComponent;
-
